328b5fd243de68cb3d2547732bf7a15b8544db1f
zulzulomga-byte <zulzulomga@gmail.com>
2026-08-30 00:10:06 +0900

    build apk

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vyra.player',
+  appName: 'Vyra Player',
+  webDir: 'www'
+};
+
+export default config;
