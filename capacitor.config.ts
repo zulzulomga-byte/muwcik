3ef7288b51c739e802882b8623c916db9370eee6
zulzulomga-byte <zulzulomga@gmail.com>
2026-08-30 06:55:25 +0900

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.vyra.player',
-  appName: 'Vyra Player',
-  webDir: 'www'
-};
-
-export default config;
